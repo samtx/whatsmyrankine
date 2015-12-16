5fe12a578b23eee03a4380ce6e3dff301fd252da
Sam Friedman <samfriedman@tamu.edu>
2015-12-16 23:02:44 +0000

    add print os.environ

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 runner = Runner(app)
 
+print(os.environ['APP_SETTINGS'])
+
 @app.route('/')
 def hello():
     return "Hello World!"
