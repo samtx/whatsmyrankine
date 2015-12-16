2de526951fa9bd278a77f0433f0275d3686eefae
Sam Friedman <samfriedman@tamu.edu>
2015-12-16 22:35:30 +0000

    add hello_name function

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
 if __name__=='__main__':
     app.run(host='0.0.0.0')
 
