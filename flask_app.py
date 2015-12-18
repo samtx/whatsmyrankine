83d240ca4e891ab2a76c0a94f9368d98660066a6
Sam Friedman <samfriedman@tamu.edu>
2015-12-18 07:40:20 +0000

    remove runner flask extension so that it will work in pythonanywhere

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,14 @@
 from flask import Flask
-from flask.ext.runner import Runner
+#from flask.ext.runner import Runner
 import os
 from app import app
 
 #app.config.from_object(os.environ['APP_SETTINGS'])
-runner = Runner(app)
+#runner = Runner(app)
 
 #print(os.environ['APP_SETTINGS'])
 
 if __name__=='__main__':
-    runner.run()
+    app.run()
+    #runner.run()
 
